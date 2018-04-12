0d24ff3ec9d1bb065391f648ba78c882ec4d82ae
Mingye <Mingye@users.noreply.github.com>
2018-04-12 12:51:13 -0700

    Initial version of assume_role(), validate_role(), download_object(), download_upload_object(), get_glacier_metadata(), and list_objects() created on March 12, 2018

---
+++ mys3.py
@@ -0,0 +1,75 @@
+import boto3, tempfile, re
+from datetime import datetime, timezone
+from dateutil import parser
+
+
+def assume_role(role):
+    sts = boto3.client('sts')
+    role = sts.assume_role(RoleArn=role, RoleSessionName='assumed-role')
+    client = boto3.client(
+        's3',
+        aws_access_key_id = role['Credentials']['AccessKeyId'],
+        aws_secret_access_key = role['Credentials']['SecretAccessKey'],
+        aws_session_token = role['Credentials']['SessionToken'],
+    )
+    client.expiration = role['Credentials']['Expiration']
+    return client
+
+def validate_role(client, role, timedelta):
+    difference = client.expiration - datetime.now(timezone.utc)
+    if difference < timedelta:
+        return assume_role(role)
+    else:
+        return client
+
+
+def download_object(client, bucket, key, file, range_=None):
+    if range_ is None:
+        response = client.get_object(Bucket=bucket, Key=key)
+    else:
+        response = client.get_object(Bucket=bucket, Key=key, Range=range_)
+    file.write(response['Body'].read())
+    file.seek(0)
+
+def download_upload_object(client1, bucket1, key1, client2, bucket2, key2, range_=None):
+    with tempfile.TemporaryFile() as tf:
+        if range_ is None:
+            response = client1.get_object(Bucket=bucket1, Key=key1)
+        else:
+            response = client1.get_object(Bucket=bucket1, Key=key1, Range=range_)
+        tf.write(response['Body'].read())
+        tf.seek(0)
+        client2.upload_fileobj(tf, bucket2, key2)
+
+
+ONGOING_TEMPLATE = re.compile(r'ongoing-request="(?P<ongoing>true|false)"')
+EXPIRY_TEMPLATE = re.compile(r'expiry-date="(?P<expiry>.+)"')
+
+def get_glacier_metadata(client, bucket, key):
+    response = client.head_object(Bucket=bucket, Key=key)
+    size, storage = response['ContentLength'], response['StorageClass']
+    if 'Restore' in response:
+        restore = response['Restore']
+        ongoing = ONGOING_TEMPLATE.search(restore).group('ongoing') == 'true'
+        if not ongoing:
+            expiry = parser.parse(EXPIRY_TEMPLATE.search(restore).group('expiry'))
+            return size, storage, ongoing, expiry
+        else:
+            return size, storage, ongoing, None
+    else:
+        return size, storage, False, None
+
+
+def _extract_fields_from_response(response, fields):
+    return [tuple(content.get(field) for field in fields)
+            for content in response['Contents']]
+
+def list_objects(client, bucket, prefix, fields):
+    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    results = _extract_fields_from_response(response, fields)
+    while response['IsTruncated']:
+        nct = response['NextContinuationToken']
+        response = client.list_objects_v2(Bucket=bucket, Prefix=prefix,
+                                          ContinuationToken=nct)
+        results.extend(_extract_fields_from_response(response, fields))
+    return results
